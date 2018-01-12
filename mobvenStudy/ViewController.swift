36ba234a9b691c0ddc447161f5d10d7ff2389562
Emre YILMAZ <eyilmaz@fatma-imac.local>
2018-01-12 11:28:24 +0300

    Initial Commit

---
+++ mobvenStudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mobvenStudy
+//
+//  Created by EMRE YILMAZ on 12.01.2018.
+//  Copyright © 2018 YUNUS YILMAZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
